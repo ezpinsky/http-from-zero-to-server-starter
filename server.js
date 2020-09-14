12187db1a65a7bff4002d1339ee3a289ad9683fa
Corbin Armendariz <corbinarmendariz@gmail.com>
2020-09-14 16:13:21 -0500

    phase 2 done

--- server.js
+++ server.js
@@ -5,11 +5,43 @@ http.createServer(function (req, res) {
 		console.log("Inbound 'OK' request being processed...");
 		res.writeHead(200);
 		res.end();
-	} else {
-		console.log("go find it somehwere else");
+	} if (req.url === "/Bad-Request") {
+		// console.log("Inbound 'OK' request being processed...");
+		res.writeHead(400);
+		res.end();
+	} if (req.url === "/Created") {
+		// console.log("Inbound 'OK' request being processed...");
+		res.writeHead(201);
+		res.end();
+	} if (req.url === "/Forbidden") {
+		// console.log("Inbound 'OK' request being processed...");
+		res.writeHead(403);
+		res.end();
+	} if (req.url === "/Found") {
+		// console.log("Inbound 'OK' request being processed...");
+		res.writeHead(302);
+		res.end();
+	} if (req.url === "/Gateway-Timeout") {
+		// console.log("Inbound 'OK' request being processed...");
+		res.writeHead(504);
+		res.end();
+	} if (req.url === "/Internal-Server-Error") {
+		// console.log("Inbound 'OK' request being processed...");
+		res.writeHead(500);
+		res.end();
+	} if (req.url === "/Moved-Permanently") {
+		// console.log("Inbound 'OK' request being processed...");
+		res.writeHead(301);
+		res.end();
+	} if (req.url === "/Unauthorized") {
+		// console.log("Inbound 'OK' request being processed...");
+		res.writeHead(401);
+		res.end();
+	} if (req.url === '/Not-Found') {
+		console.log("go find it somewhere else");
 		res.writeHead(404);
 		res.end();
 	}
 }).listen(3000, function () {
-	console.log("!this project is lame || I didnt forget the port number");
+	console.log("!this project is lame || I didn't forget the port number");
 });
